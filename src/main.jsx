7cea39573200b61fcbea8ca657696fae8ffbcedd
Anhelina <fadeevalina20@gmail.com>
2024-10-04 11:42:25 +0300

    add

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
-import { store, persistor } from './redux/store/store';
+import { store, persistor } from './redux/store';
 import App from './App';
 
 // Получаем корень элемента
