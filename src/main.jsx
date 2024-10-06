65577692398ec22885dff1846cd2b2af8306b985
Anhelina <fadeevalina20@gmail.com>
2024-10-06 17:55:17 +0300

    add

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,13 @@ import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import App from "./App.jsx";
 import "./index.css";
-import { store, persistor } from "./redux/store.js"; // Убедитесь, что здесь правильный импорт
+import { store, persistor } from "./redux/store.js";
 import { PersistGate } from "redux-persist/integration/react";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
-      <PersistGate persistor={persistor}> {/* Здесь должна быть переменная 'persistor', а не 'persistore' */}
+      <PersistGate persistor={persistor}> {}
         <App />
       </PersistGate>
     </Provider>
